95380286518f3be1cb8b38ec6a7d115c85941f64
team-o-wuk <147349147+team-o-wuk@users.noreply.github.com>
2024-04-17 10:36:07 +0300

    make
    added GNUMake file

--- main.cpp
+++ main.cpp
@@ -4,40 +4,40 @@
 int main()
 {
     
-    Matrix a(4, 4);
+    Matrix a(2, 2);
     a.get_matrix();
 
-    Matrix b(4, 4);
+    Matrix b(2, 2);
     b.get_matrix();
 
-    Matrix c(2, 3);
-    c.get_matrix();
+    // Matrix c(2, 3);
+    // c.get_matrix();
 
     (a+b).print_matrix();
-    (a+c).print_matrix();
+    // (a+c).print_matrix();
 
-    (a-b).print_matrix();
-    (a-c).print_matrix();
+    // (a-b).print_matrix();
+    // (a-c).print_matrix();
 
-    (a*b).print_matrix();
-    (a*c).print_matrix();    
-    (a*2).print_matrix();
-    (c*0.5).print_matrix();
+    // (a*b).print_matrix();
+    // (a*c).print_matrix();    
+    // (a*2).print_matrix();
+    // (c*0.5).print_matrix();
 
-    std::cout << (a==b) << std::endl;
-    std::cout << (a!=c) << std::endl;
+    // std::cout << (a==b) << std::endl;
+    // std::cout << (a!=c) << std::endl;
   
-    a.change_st(2, 3);
-    a.change_ft(1, 2);
-    a.change_tt(3, 2, 2);
-    a.print_matrix();
+    // a.change_st(2, 3);
+    // a.change_ft(1, 2);
+    // a.change_tt(3, 2, 2);
+    // a.print_matrix();
    
-    Matrix A(3, 3);
-    A.get_matrix();
+    // Matrix A(3, 3);
+    // A.get_matrix();
  
-    std::cout << A.determinant() << std::endl;
-    (A*2).print_matrix();
-    A.reversive().print_matrix();
+    // std::cout << A.determinant() << std::endl;
+    // (A*2).print_matrix();
+    // A.reversive().print_matrix();
     
 
     return 0;
