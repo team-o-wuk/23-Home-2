831f2c181915fc28c83fb97097c46ae680de22c6
team-o-wuk <147349147+team-o-wuk@users.noreply.github.com>
2024-04-21 14:46:09 +0300

    testing
    editetd main
    tested ! operator

--- main.cpp
+++ main.cpp
@@ -7,33 +7,30 @@
 
 int main()
 {
-    // Matrix n;
-    // std::cin >> n;
-    // n.print_matrix();
+     
 
-    // Matrix m(3,3);
-    // Matrix m;
+    Matrix m(3,3);
+  
 
-    // std::ifstream in("in.txt");
-    // if (in.is_open())
-    // {
-    //     in >> m;
-    //     in.close();
-    // }
+    std::ifstream in("in.txt");
+    if (in.is_open())
+    {
+        in >> m;
+        in.close();
+    }
 
     
-    // m.print_matrix();
-    // std::cout << m;
+    Matrix r = !m;
 
-    // if (m == 3) std::cout << "lol yes";
-    // else std::cout << "noooo";
-    // std::cout << (m == 3);
-    // std::ofstream f("out.txt");
-    // if (f.is_open())
-    // {
-    //     f << m;
-    //     f.close();
-    // }
+    std::cout << m;
+    std::cout << r;
+
+    Matrix s = m+r;
+    std::cout << s;
+
+    std::ofstream out("out.txt");
+    out << s;
+    out.close();
 
     Matrix z = Matrix::zero(1, 5);
     std::cout << z;
@@ -41,40 +38,7 @@ int main()
     Matrix u = Matrix::unit(4);
     std::cout << u;
 
-    // Matrix a(2, 2);
-    // a.get_matrix();
-
-    // Matrix b(2, 2);
-    // b.get_matrix();
-
-    // Matrix c(2, 3);
-    // c.get_matrix();
-
-    // (a+b).print_matrix();
-    // (a+c).print_matrix();
-
-    // (a-b).print_matrix();
-    // (a-c).print_matrix();
-
-    // (a*b).print_matrix();
-    // (a*c).print_matrix();    
-    // (a*2).print_matrix();
-    // (c*0.5).print_matrix();
-
-    // std::cout << (a==b) << std::endl;
-    // std::cout << (a!=c) << std::endl;
-  
-    // a.change_st(2, 3);
-    // a.change_ft(1, 2);
-    // a.change_tt(3, 2, 2);
-    // a.print_matrix();
-   
-    // Matrix A(3, 3);
-    // A.get_matrix();
  
-    // std::cout << A.determinant() << std::endl;
-    // (A*2).print_matrix();
-    // A.reversive().print_matrix();
     
 
     return 0;
